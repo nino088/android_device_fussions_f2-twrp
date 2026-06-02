778739499b4022c84a5fb1b0dcbfc87d0f25dcbc
nino <pilopilosimon1990@gmail.com>
2026-06-02 23:33:41 +0100

    Update and rename omni_s3912e.mk to twrp_s3912e.mk

--- twrp_s3912e.mk
+++ twrp_s3912e.mk
@@ -1,21 +1,26 @@
 #
 # Copyright (C) 2026 The Android Open Source Project
-# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Inherit from s3912e device
 $(call inherit-product, device/alps/s3912e/device.mk)
 
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
 PRODUCT_DEVICE := s3912e
-PRODUCT_NAME := omni_s3912e
+PRODUCT_NAME := twrp_s3912e
 PRODUCT_BRAND := alps
 PRODUCT_MODEL := F2
 PRODUCT_MANUFACTURER := alps
